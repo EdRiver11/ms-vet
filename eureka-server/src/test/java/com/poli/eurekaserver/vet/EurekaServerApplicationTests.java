71fa0c716292544bf600eb4bd97512a9f751ba9f
edwin <dwein08@gmail.com>
2025-05-17 12:14:30 -0500

    Creando el servidor eureka

---
+++ eureka-server/src/test/java/com/poli/eurekaserver/vet/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poli.eurekaserver.vet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
